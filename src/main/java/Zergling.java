e3d21f05864d75982b46f1144f4bba7fe692f8a2
ariari01 <juh.shin@gmail.com>
2025-06-06 10:54:25 +0900

    teacherCode

--- src/main/java/Zergling.java
+++ src/main/java/Zergling.java
@@ -1,18 +1,36 @@
-public class Zergling {
-    int hp = 80;
-    int mana = 200;
+class Zergling {
+    public int hp;
+    public int mana;
 
-    void attack() {
+    public Zergling() {
+        this.hp = 80;
+        this.mana = 200;
+    }
+
+    public void attack() {
         hp += 1;
         mana -= 10;
     }
 
-    void move() {
+    public void move() {
         hp -= 10;
         mana += 5;
     }
 
-    void status() {
-        System.out.println("현재 HP는 " + hp + " MANA는 " + mana + " 입니다.");
+    public void status() {
+        System.out.println(hp + " " + mana);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Zergling z1 = new Zergling();
+        Zergling z2 = new Zergling();
+
+        z1.hp = 10;
+        z2.mana = 20;
+
+        z1.attack();
+        z2.attack();
     }
 }
